7a0d811b11af1e686a555b7405d05417222c0904
vozio <vozio@voziodeMac-mini.local>
2018-11-14 10:44:56 +0800

    1

--- Flag/AppDelegate.swift
+++ Flag/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
         window?.makeKeyAndVisible()
-        
         window?.rootViewController = UINavigationController.init(rootViewController:  MainPage())
         return true
     }
